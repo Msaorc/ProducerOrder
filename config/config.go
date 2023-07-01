ab154559c55a8e90b2fe15dd4420f3d7a845d52b
Marcos Augusto <msaorc@hotmail.com>
2023-07-01 12:53:21 -0300

    Inicio da criação das configurações do postador de pedidos

--- config/config.go
+++ config/config.go
@@ -3,20 +3,20 @@ package config
 import "github.com/spf13/viper"
 
 type configRabbitMq struct {
-	HOST string `mapstructure:"HOST"`
-	USER string `mapstructure:"USER"`
+	HOST     string `mapstructure:"HOST"`
+	USER     string `mapstructure:"USER"`
 	PASSWORD string `mapstructure:"PASSWORD"`
 }
 
-func LoadConfigs(path string) (*configRabbitMq){
+func LoadConfigs(path string) *configRabbitMq {
 	var config *configRabbitMq
 	viper.SetConfigName("rabbit_confg")
 	viper.SetConfigType("env")
 	viper.AddConfigPath(path)
 	viper.SetConfigFile(".env")
 	viper.AutomaticEnv()
+	return config
 }
-
 
 // var cfg *conf
 // viper.SetConfigName("app_config")
