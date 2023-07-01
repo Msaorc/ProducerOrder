e9ebebebe69f70092b3dd548c0d6b17a47675414
Marcos Augusto <msaorc@hotmail.com>
2023-07-01 12:51:38 -0300

    Inicio da criação das configurações do postador de pedidos

--- config/config.go
+++ config/config.go
@@ -1,5 +1,36 @@
 package config
 
-func LoadConfigs(path string) {
+import "github.com/spf13/viper"
 
+type configRabbitMq struct {
+	HOST string `mapstructure:"HOST"`
+	USER string `mapstructure:"USER"`
+	PASSWORD string `mapstructure:"PASSWORD"`
 }
+
+func LoadConfigs(path string) (*configRabbitMq){
+	var config *configRabbitMq
+	viper.SetConfigName("rabbit_confg")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+}
+
+
+// var cfg *conf
+// viper.SetConfigName("app_config")
+// viper.SetConfigType("env")
+// viper.AddConfigPath(path)
+// viper.SetConfigFile(".env")
+// viper.AutomaticEnv()
+// err := viper.ReadInConfig()
+// if err != nil {
+// 	panic(err)
+// }
+// err = viper.Unmarshal(&cfg)
+// if err != nil {
+// 	panic(err)
+// }
+// cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JwtSecret), nil)
+// return cfg, nil
